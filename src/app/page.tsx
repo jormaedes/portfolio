da407618055dd2c498e81600f3d8adcdc13ecf86
Jormaedes Luís <jormahed@gmail.com>
2026-09-24 15:17:02 +0100

    feat: add Contact component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/About"
 import Services from "@/components/Services";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutSection />
       <Services />
       <Projects />
+      <Contact />
     </main>
   );
 }
